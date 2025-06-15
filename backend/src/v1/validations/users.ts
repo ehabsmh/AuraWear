ca89206a79370c2832c85da08b445664d220ddec
Ehab Hussein <ehabsmh3@gmail.com>
2025-06-15 16:53:07 +0300

    Initial user's data validation

---
+++ backend/src/v1/validations/users.ts
@@ -0,0 +1,18 @@
+import Joi from "joi";
+
+const userValidationSchema = Joi.object({
+  firstName: Joi.string().min(3).max(15).required(),
+  lastName: Joi.string().min(3).max(15).required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().alphanum().min(6).max(30).required(),
+  confirmPassword: Joi.ref("password"),
+  birthDate: Joi.date().min("1-1-1930").max("now").required(),
+  city: Joi.string().min(3).max(30).required(),
+  address: Joi.string().min(3).max(100).required(),
+  postalCode: Joi.string().min(3).max(6).required(),
+  phone: Joi.string()
+    .pattern(/^01[0125]\d{8}$/)
+    .required(),
+});
+
+export default userValidationSchema;
